accbe1ad5032487f05ed1e79da5e69fad5e638e9
Martin <silverball1@gmail.com>
2024-01-26 13:07:13 +0100

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 
+interface Pokemon {
+  id: number;
+  name: string;
+  type: string;
+  isCool: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
- pokemonName: String = "";
+pokemons: Pokemon[] = [{
+  id : 1,
+  name: 'pikachu',
+  type: 'electrik',
+  isCool: false
+},{
+  id : 2,
+  name: 'squirtle',
+  type: 'water',
+  isCool: true
+},{
+  id : 3,
+  name: 'charmander',
+  type: 'fire',
+  isCool: true
+}]
+
 
  constructor() { } 
 
 
- handleChange(event : any){
-  this.pokemonName = event?.target.value;
- }
 }
