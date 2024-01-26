4ee5d15b3a09e8e2eff2509fd91e4458d43253c4
Martin <silverball1@gmail.com>
2024-01-26 16:23:22 +0100

    ngoninit

---
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrl: './pokemon-list.component.css'
+})
+export class PokemonListComponent implements OnInit{
+
+  pokemon: string;
+
+  constructor() {
+    this.pokemon = '';
+   }
+
+  ngOnInit(): void {
+    console.log('PokemonListComponent initialized');
+    this.pokemon = '';
+  }
+
+}
