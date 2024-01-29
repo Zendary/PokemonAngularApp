0f1289197c8f55f296653114aec6c07f596838f6
Martin <silverball1@gmail.com>
2024-01-29 14:41:56 +0100

    ngform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonBaseModule } from './pokemon-base/pokemon-base.module';
+import { PokemonTemplateFormComponent } from './pokemon-template-form/pokemon-template-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonTemplateFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     PokemonBaseModule
   ],
